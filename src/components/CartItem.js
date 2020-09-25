c41826badbabffe195297c630d98be3c6ba08e4a
ihsan-aryandi <ihsan.webfront@gmail.com>
2020-09-25 08:53:27 +0700

    Menambahkan harga pada Component CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 import './css/CartItem.css'
 
-export default function CartItem({ productPicture, quantity, productName, onIncrement, onDecrement }) {
+export default function CartItem({ productPicture, productPrice, quantity, productName, onIncrement, onDecrement }) {
     return (
         <div className="cart-item">
             <div className="image">
                 <img src={productPicture} alt="asd"/>
             </div>
-            <h4>{productName} <span>({quantity})</span></h4>
+            <div className="product">
+                <h4>{productName} <span>({quantity})</span></h4>
+                <small>Rp. {productPrice * quantity}</small>
+            </div>
             <div className="cta">
                 <div className="minus" onClick={onDecrement}>-</div>
                 <div className="plus" onClick={onIncrement}>+</div>
